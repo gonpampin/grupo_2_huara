e1cbec9c3a6b6cead07abd7cca8cf546d7ab0b02
bernardo607 <bernardo.segui@gmail.com>
2021-09-15 20:42:31 -0300

    inicio del proyecto

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+/* Config public */
+app.use(express.static(path.resolve(__dirname, 'public')));
+
+
+/* Routes */
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'views/home.html'));
+})
+
+app.get('/productos', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'views/product.html'));
+})
+app.get('/carrito', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'views/productCart.html'));
+})
+app.get('/login', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'views/login.html'));
+})
+app.get('/registro', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'views/register.html'));
+})
+
+
+
+app.listen(3000, () => console.log('Server corriendo en el puerto 3000'));
---
+++ retro.md
@@ -0,0 +1,15 @@
+# Proyecto Huara #
+
+
+## *Sprint 1* ##
+
+### ** Hacer más ** ###
+Usar más slack como canal de comunicación
+
+### ** Continuar haciendo ** ###
+Reuniones semanales
+
+### ** Hacer menos ** ###
+Usar menos whatsapp como canal de comunicación
+
+
