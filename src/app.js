a36b642713efee04e796a02a2648699bffd37acd
gonpampin <gonpampin@gmail.com>
2021-10-19 18:19:28 -0300

    link logo

--- src/app.js
+++ src/app.js
@@ -5,12 +5,11 @@ const path = require('path');
 
 const router = require('./routes/mainRoutes');
 
-/* Config public */
+
 app.use(express.static(path.resolve(__dirname, '../public')));
 
 
-// view engine setup
-/*app.set('views', path.resolve(__dirname, 'views'));*/
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
@@ -20,39 +19,3 @@ app.listen(3000, () => {
     console.log('Servidor levantado en el puerto 3000')
 })
 
-/* POST
-app.use(express.urlencoded({extended: false}));
-app.use(express.json());
-
-/* Routes 
-app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'views/home.ejs'));
-})
-
-app.get('/productos', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'views/product.html'));
-})
-app.get('/carrito', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'views/productCart.html'));
-})
-app.get('/login', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'views/login.html'));
-})
-app.get('/registro', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'views/register.html'));
-})
-
-app.post('/login', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'views/home.html'));
-})
-app.post('/register', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'views/home.html'));
-})
-
-
-
-
-
-app.listen(3000, () => console.log('Server corriendo en el puerto 3000'));
-
-*/
