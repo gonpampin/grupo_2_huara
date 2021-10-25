0d8fe1ea88cf13a63e7dceb448daaf39505603b6
victoriacutino <victoria.cutino@gmail.com>
2021-10-25 19:07:23 -0300

    ruta-post-crearproducto

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -4,6 +4,15 @@ const fs = require('fs');
 let jsonProducts = fs.readFileSync(path.resolve(__dirname, '../db/product.json'), 'utf-8');
 let productosLista = JSON.parse(jsonProducts); 
 
+const nuevoId = () => {
+    let ultimo = 0;
+    products.forEach(product => {
+        if (product.id > ultimo) {
+            ultimo = product.id;
+        }
+    });
+    return ultimo + 1;
+}
 
 let productController = {
 
@@ -27,8 +36,23 @@ let productController = {
         })
 
         res.render('products/productDetail', { products: productoDetalle });
+    },
+
+    store: (req, res) => {
+        let product = {
+            id: nuevoId(),
+            ...req.body,
+             image: req.file.filename || 'default-image.png',
+        }
+        products.push(product);
+
+        let jsonDeProductos = JSON.stringify(products, null, 4);
+        fs.writeFileSync(path.resolve(__dirname, '../db/products.json'), jsonDeProductos);
+
+        res.redirect('/');
+
+
     }
-    
 
 }    
 
--- src/routes/productsRoutes.js
+++ src/routes/productsRoutes.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const router = express.Router();
 const path = require('path');
+const multer = require ('multer');
 
 
 const productController = require('../controllers/productsController');
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, path.resolve(__dirname, '../../public/images/products'));
+    },
+    filename: function (req, file, cb) {
+        const name = `product-${Date.now()}${path.extname(file.originalname)}`
+        cb(null, name)
+    }
+})
+  
+const upload = multer({ storage })
 
 router.get('/', productController.product);
 
@@ -12,6 +24,9 @@ router.get('/crearproducto', productController.formCreate);
 router.get('/editarproducto', productController.formEdit);
 
 router.get('/:id', productController.detail);
+
+
+router.post('/crearproducto', upload.single('image'), productController.store);
 
 
 module.exports = router
