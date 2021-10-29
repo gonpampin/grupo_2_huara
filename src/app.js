b00d2b80b1237182ac4d93af8d392a33a0a1a8a7
ignacioiriondo <nachoiriondo90@gmail.com>
2021-10-29 17:21:14 -0300

    ruta editar producto

--- src/app.js
+++ src/app.js
@@ -13,6 +13,7 @@ const usersRouter = require('./routes/usersRoutes');
 
 app.use(express.static(path.resolve(__dirname, '../public')));
 
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json())
 
 
