20398ef21cb7cffb9420fe2a63bd4bc9cc4eab5b
gonpampin <gonpampin@gmail.com>
2021-12-30 20:04:30 -0300

    front de borrar

--- public/js/products.js
+++ public/js/products.js
@@ -1,6 +1,26 @@
+let formDelete = document.querySelectorAll(".formDeleteJS")
+formDelete.forEach(deleteJS => {
+    deleteJS.addEventListener("submit", (e)=> {
+        e.preventDefault();
+        swal({
+            title: "Estas por eliminar el producto",
+            text: "Una vez borrado no podras recuperarlo!",
+            icon: "warning",
+            buttons: true,
+            dangerMode: true,
+          })
+          .then((resultado) => {
+            if (!resultado) {
+              swal("El producto no se borrara!", {
+                icon: "success",
+              });
+            } else {
+              deleteJS.submit();
+            }
+          });
+    })
+})
 
-/*et rojo = document.querySelector(".product")
-rojo.style.backgroundColor = "red"
-console.log(rojo)*/
+
 
 
