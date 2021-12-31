2adbf839c69b41a54404c153ef05033f3ebb8eb4
gonpampin <gonpampin@gmail.com>
2021-12-31 11:54:15 -0300

    ojo password casi ok

--- public/js/ojoPassword.js
+++ public/js/ojoPassword.js
@@ -3,5 +3,13 @@ let input = document.querySelector('#password')
 let ojo = document.querySelector('#ojo')
 
 ojo.addEventListener('click', ()=>{
-    console.log(input.type)
-})
+    if(input.type == 'password'){
+        ojo.classList.remove('fa-eye-slash');
+        ojo.classList.add('fa-eye');
+        input.type = 'text';
+    }else{
+        ojo.classList.remove('fa-eye');
+        ojo.classList.add('fa-eye-slash');
+        input.type = 'password';
+    }
+});
