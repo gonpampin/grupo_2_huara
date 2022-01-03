0fdb3f18e4e44ea1bb3ba76dbfbc7a01804a8311
bernardo607 <bernardo.segui@gmail.com>
2022-01-03 17:43:31 -0300

    validacion front registro

--- public/js/ojoPassword.js
+++ public/js/ojoPassword.js
@@ -1,13 +1,12 @@
-
 let input = document.querySelector('#password')
 let ojo = document.querySelector('#ojo')
 
-ojo.addEventListener('click', ()=>{
-    if(input.type == 'password'){
+ojo.addEventListener('click', () => {
+    if (input.type == 'password') {
         ojo.classList.remove('fa-eye-slash');
         ojo.classList.add('fa-eye');
         input.type = 'text';
-    }else{
+    } else {
         ojo.classList.remove('fa-eye');
         ojo.classList.add('fa-eye-slash');
         input.type = 'password';
@@ -16,14 +15,65 @@ ojo.addEventListener('click', ()=>{
 let inputRe = document.querySelector('#passwordRe')
 let ojoRe = document.querySelector('#ojoRe')
 
-ojoRe.addEventListener('click', ()=>{
-    if(inputRe.type == 'password'){
+ojoRe.addEventListener('click', () => {
+    if (inputRe.type == 'password') {
         ojoRe.classList.remove('fa-eye-slash');
         ojoRe.classList.add('fa-eye');
         inputRe.type = 'text';
-    }else{
+    } else {
         ojoRe.classList.remove('fa-eye');
         ojoRe.classList.add('fa-eye-slash');
         inputRe.type = 'password';
     }
+})
+
+/* validacion de form*/
+const regexValidationEmail =/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+let form = document.querySelector('#registerForm')
+
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    /*validacion de nombre*/
+    if (form.name.value == '') {
+        swal('El nombre no puede estar vacío')
+    } else if (form.name.value.length < 2) {
+        swal('El nombre debe tener al menos dos caracteres')
+
+        /*validacion de apellido*/
+    } else if (form.lastname.value == '') {
+        swal('El apellido no puede estar vacío')
+    } else if (form.lastname.value.length < 2) {
+        swal('El apellido debe tener al menos dos caracteres')
+
+        //validacion de email
+    } else if (form.email.value == '') {
+        swal('El email no puede estar vacío')
+    }else if (!(regexValidationEmail.test(form.email.value))) {
+            swal('El email debe tener un formato válido')
+    //validacion de imagen
+    } else if (form.avatar.value == '') {
+        swal('Debes cargar una imagen')
+    /*} else if (form.avatar.file.includes(".jpg") ||
+                form.avatar.file.includes(".png") ||
+                form.avatar.file.includes(".gif") || 
+                form.avatar.file.includes(".jpeg") ) {
+        swal('Debes cargar una imagen en formato ".jpg", ".png", ".gif",".jpeg"')*/
+    //validacion de password
+    } else if (form.contrasenia.value == '') {
+        swal('La contraseña no puede estar vacía')
+    } else if (form.contrasenia.value.length < 8) {
+        //usar REGEX
+        swal('La contraseña debe tener al menos 8 caracteres')
+     
+        //validacion de repassword
+    } else if (form.recontrasenia.value != form.contrasenia.value) {
+        //usar REGEX
+        swal('Las contraseñas no coinciden')
+    } else {
+        form.submit()
+    }
+
+    
 })
--- public/js/products.js
+++ public/js/products.js
@@ -3,15 +3,15 @@ formDelete.forEach(deleteJS => {
     deleteJS.addEventListener("submit", (e)=> {
         e.preventDefault();
         swal({
-            title: "Estas por eliminar el producto",
-            text: "Una vez borrado no podras recuperarlo!",
+            title: "Estás por eliminar el producto",
+            text: "¡Una vez borrado no podrás recuperarlo!",
             icon: "warning",
             buttons: true,
             dangerMode: true,
           })
           .then((resultado) => {
             if (!resultado) {
-              swal("El producto no se borrara!", {
+              swal("¡El producto no se borrará!", {
                 icon: "success",
               });
             } else {
