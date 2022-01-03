092db4ee11816674d94995adf8abb6d9f1c54437
gonpampin <gonpampin@gmail.com>
2022-01-03 15:44:10 -0300

    eye arreglado

--- public/js/ojoPassword.js
+++ public/js/ojoPassword.js
@@ -13,3 +13,17 @@ ojo.addEventListener('click', ()=>{
         input.type = 'password';
     }
 });
+let inputRe = document.querySelector('#passwordRe')
+let ojoRe = document.querySelector('#ojoRe')
+
+ojoRe.addEventListener('click', ()=>{
+    if(inputRe.type == 'password'){
+        ojoRe.classList.remove('fa-eye-slash');
+        ojoRe.classList.add('fa-eye');
+        inputRe.type = 'text';
+    }else{
+        ojoRe.classList.remove('fa-eye');
+        ojoRe.classList.add('fa-eye-slash');
+        inputRe.type = 'password';
+    }
+})
