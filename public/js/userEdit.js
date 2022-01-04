69871ed874c499b59f8b02b33494c57ecf78ca9f
victoriacutino <victoria.cutino@gmail.com>
2022-01-04 14:42:53 -0300

    editUserValidation

--- public/js/userEdit.js
+++ public/js/userEdit.js
@@ -3,20 +3,41 @@ let spanName=document.querySelector('#spanEdit')
 editUserFront.addEventListener("submit", function(e){
             e.preventDefault()
         let errors={};
+
         if(editUserFront.name.value.length==0){
             errors.spanEdit='El campo de nombre debe estar completo'
         spanName.innerText=errors.spanEdit
-        } else if(editUserFrontname.value.length)
-        if(editUserFront.name.value.length==0){
-            errors.spanEdit='El campo de nombre debe estar completo'
+        } else if(editUserFront.name.value.length<2){
+            errors.spanEdit='El campo de nombre debe tener al menos 2 caracteres'
+            spanName.innerText=errors.spanEdit
+        } else{
+            delete errors.spanEdit
+            spanName.innerText=""
+        }
+
+
+
+        if(editUserFront.lastname.value.length==0){
+            errors.spanEdit='El campo de apellido debe estar completo'
         spanName.innerText=errors.spanEdit
+        } else if(editUserFront.lastname.value.length<2){
+        errors.spanEdit='El campo de apellido debe tener al menos 2 caracteres'
+        }
+        if(editUserFront.email.value.length==0){
+            errors.spanEdit='El campo de email debe estar completo'
+        spanName.innerText=errors.spanEdit
+        } else if(editUserFront.lastname.value.length<2){
+            errors.spanEdit='El campo de apellido debe tener al menos 2 caracteres'
         }
         if(editUserFront.name.value.length==0){
             errors.spanEdit='El campo de nombre debe estar completo'
         spanName.innerText=errors.spanEdit
-        }
-        if(editUserFront.name.value.length==0){
-            errors.spanEdit='El campo de nombre debe estar completo'
-        spanName.innerText=errors.spanEdit
-        }
+   
+    }
+
+    if(Object.keys(errors).length == 0){
+        editUserFront.submit();
+    }
+
 })
+
