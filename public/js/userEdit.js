99da6b6f15d0cfd09dbae23e6890b791e3e215f5
bernardo607 <bernardo.segui@gmail.com>
2022-01-05 16:17:29 -0300

    cosas menores

--- public/js/userEdit.js
+++ public/js/userEdit.js
@@ -6,27 +6,31 @@ window.addEventListener('load', function(){
 let editUserFront= document.querySelector("#editUserFront")
 
 editUserFront.addEventListener("submit", function(e){
-    let errors=[]    
+    let errors={
+        name: [],
+        lastname: []
+
+        }
    
         
         let campoNombre=document.querySelector('input#name')
 
         if(campoNombre.value.length==0){
             
-            errors.push('El campo de nombre debe estar completo')
+            errors.name.push('El campo de nombre debe estar completo')
             
         } else if(campoNombre.value.length<2){
-            errors.push('El campo de nombre debe tener al menos 2 caracteres')
+            errors.name.push('El campo de nombre debe tener al menos 2 caracteres')
             
         }
 
         let campoApellido=document.querySelector('input#lastname')
 
         if(campoApellido.value.length==0){
-            errors.push('El campo de apellido debe estar completo')
+            errors.lastname.push('El campo de apellido debe estar completo')
 
         } else if(campoApellido.value.length<2){
-        errors.push('El campo de apellido debe tener al menos 2 caracteres')
+        errors.lastname.push('El campo de apellido debe tener al menos 2 caracteres')
        
         }
     
