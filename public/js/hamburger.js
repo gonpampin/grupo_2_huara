9fd3708597fe7848abcd273dba0ed43365faf0d1
gonpampin <gonpampin@gmail.com>
2022-01-12 16:25:31 -0300

    menu hamburger 1.0.0

--- public/js/hamburger.js
+++ public/js/hamburger.js
@@ -1,7 +1,18 @@
 let hamburger = document.querySelector('.fa-align-left')
 
 hamburger.addEventListener('click', (e) => {
+if (e && (document.getElementById('ham').className == 'no-desplegado')){
+    document.getElementById('ham').classList.remove('no-desplegado');
+    document.getElementById('ham').classList.add('desplegado')
+}else{
+    document.getElementById('ham').classList.remove('desplegado');
+    document.getElementById('ham').classList.add('no-desplegado')
+}
+    //Esta tambien funciona
+/*let elm = document.getElementById('ham');
 
-    
-
+if(elm.className === 'no-desplegado'){
+    elm.className = 'desplegado';
+} else {
+    elm.className = 'no-desplegado';*/
 })
