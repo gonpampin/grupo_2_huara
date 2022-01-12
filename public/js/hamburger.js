306d285a2a173f725e899b6bf8588493590040e1
gonpampin <gonpampin@gmail.com>
2022-01-12 18:46:39 -0300

    menu hamb ten points

--- public/js/hamburger.js
+++ public/js/hamburger.js
@@ -1,18 +1,11 @@
 let hamburger = document.querySelector('.fa-align-left')
 
 hamburger.addEventListener('click', (e) => {
-if (e && (document.getElementById('ham').className == 'no-desplegado')){
-    document.getElementById('ham').classList.remove('no-desplegado');
-    document.getElementById('ham').classList.add('desplegado')
-}else{
-    document.getElementById('ham').classList.remove('desplegado');
-    document.getElementById('ham').classList.add('no-desplegado')
+    let elm = document.getElementById('ham')
+if ( elm.style.display == '' || elm.style.display == 'none' ){
+    elm.style.display = 'block';
+}else {
+    elm.style.display = 'none'
 }
-    //Esta tambien funciona
-/*let elm = document.getElementById('ham');
-
-if(elm.className === 'no-desplegado'){
-    elm.className = 'desplegado';
-} else {
-    elm.className = 'no-desplegado';*/
+   
 })
