8b1621036ea009681554476b3f66288b24a7a1c2
gonpampin <gonpampin@gmail.com>
2022-02-06 09:12:03 -0300

    carrusel css home

--- public/js/slider.js
+++ public/js/slider.js
@@ -9,19 +9,18 @@ window.addEventListener('load', function(){
 		},
 		responsive: [
 			{
-			  // screens greater than >= 775px
 			  breakpoint: 450,
 			  settings: {
-				// Set to `auto` and provide item width to adjust to viewport
 				slidesToShow: 2,
 				slidesToScroll: 2
 			  }
 			},{
-			  // screens greater than >= 1024px
 			  breakpoint: 800,
 			  settings: {
 				slidesToShow: 4,
-				slidesToScroll: 4
+				slidesToScroll: 4,
+                duration: 0.25
+
 			  }
 			}
 		]
