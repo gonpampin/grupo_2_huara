885c29e869239b764df4a92d89c8c68cdfa0d038
bernardo607 <bernardo.segui@gmail.com>
2022-02-06 19:10:05 -0300

    limpieza de db

--- src/db/diagrams/data.sql
+++ src/db/diagrams/data.sql
@@ -39,7 +39,7 @@ UNLOCK TABLES;
 
 LOCK TABLES `products` WRITE;
 /*!40000 ALTER TABLE `products` DISABLE KEYS */;
-INSERT INTO `products` VALUES (14,'MARROC','Chocolate con leche y chocolate blanco con pasta de maní','marroc.jpg',1000,20,1),(15,'BORRACHITO','Chocolate semi amargo con crema de ciruelas al Malbec con dulce de leche','borrachito.jpg',1000,20,1),(16,'DULCE DE LECHE CON NUEZ','Pasta de dulce de leche y nueces, entre una capa de chocolate con leche y una capa de chocolate blanco','nuez-ddl-blanco.jpg',1000,20,1),(17,'TABLETA ALMENDRA','Chocolate amargo 70% cacao con naranja, jengibre y almendras','almendra.jpg',1000,20,1),(18,'PISTACHO','Crema de chocolate blanco con pistachos, entre dos capas de chocolate semi amargo','pistacho.jpg',1000,20,1),(19,'AVELLANAS','Chocolate amargo con crema de licor de naranja y avellanas tostadas','avellanas2.jpg',1000,20,1),(20,'PISTACHO','Crema de chocolate blanco con pistachos, entre dos capas de chocolate semi amargo','avellanas2.jpg',1000,20,1),(21,'TABLETA FRAMBUESAS','Chocolate semi amargo con frambuesas','frambuesa.jpg',1000,20,2),(22,'TABLETA FRAMBUESAS CHOCO BLANCO','Chocolate blanco con frambuesas y nibs de cacao','frambuesa-blanco.jpg',1000,20,2),(23,'FRUTOS ROJOS','Crema de chocolate semi amargo y frutos rojos, entre una capa de chocolate amargo y una de chocolate blanco','frutos-rojos.jpg',1000,20,1),(24,'COOKIES \"N\" CREAM','Chocolate blanco con Oreos relleno de Nutella','product-1640209760692.jpg',1000,20,3),(25,'WHISKACHO','Crema de chocolate belga y whisky, con nibs de cacao','whisky.jpg',1000,20,1),(26,'HUEVO TRUFADO DDL','Mucho dulce de leche entre dos capas de chocolate con leche','huevo2.jpg',1000,20,1);
+INSERT INTO `products` VALUES (1,'MARROC','Chocolate con leche y chocolate blanco con pasta de maní','marroc.jpg',1000,20,1),(2,'BORRACHITO','Chocolate semi amargo con crema de ciruelas al Malbec con dulce de leche','borrachito.jpg',1000,20,1),(3,'DULCE DE LECHE CON NUEZ','Pasta de dulce de leche y nueces, entre una capa de chocolate con leche y una capa de chocolate blan','nuez-ddl-blanco.jpg',1000,20,1),(4,'TABLETA ALMENDRA','Chocolate amargo 70% cacao con naranja, jengibre y almendras','almendra.jpg',1000,20,1),(5,'PISTACHO','Crema de chocolate blanco con pistachos, entre dos capas de chocolate semi amargo','pistacho.jpg',1000,20,1),(6,'AVELLANAS','Chocolate amargo con crema de licor de naranja y avellanas tostadas','avellanas2.jpg',1000,20,1),(7,'TABLETA FRAMBUESAS','Chocolate semi amargo con frambuesas','frambuesa.jpg',1000,20,2),(8,'TABLETA FRAMBUESAS CHOCO BLANCO','Chocolate blanco con frambuesas y nibs de cacao','frambuesa-blanco.jpg',1000,20,2),(9,'FRUTOS ROJOS','Crema de chocolate semi amargo y frutos rojos, entre una capa de chocolate amargo y una de chocolate','frutos-rojos.jpg',1000,20,1),(10,'COOKIES \"N\" CREAM','Chocolate blanco con Oreos relleno de Nutella','huevo1.jpg',1000,20,3),(12,'HUEVO TRUFADO DDL','Mucho dulce de leche entre dos capas de chocolate con leche','huevo2.jpg',1000,20,3);
 /*!40000 ALTER TABLE `products` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -49,7 +49,7 @@ UNLOCK TABLES;
 
 LOCK TABLES `products_categories` WRITE;
 /*!40000 ALTER TABLE `products_categories` DISABLE KEYS */;
-INSERT INTO `products_categories` VALUES (1,'Tableta'),(2,'Bariloche'),(3,'Huevo de chocolate');
+INSERT INTO `products_categories` VALUES (1,'Tableta'),(2,'Bariloche'),(3,'Huevo');
 /*!40000 ALTER TABLE `products_categories` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -59,7 +59,7 @@ UNLOCK TABLES;
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'BERNARDO','www','$2a$10$YroBwbanSRy4EtgU69rHguJL3.EjyT4Yn6g8s0VRB.byjWoby.KAe','bernardo@bernardo.com','user-1639840404164_img.jpg',2),(4,'qqq','qqqq','$2a$10$LYU9Q0ubyQY8fa6omZYqSOIOHVxKJdd4IYVUUh9gaVbMo.Y33Tq.S','bernardo.segui@g.com','user-1639840580820_img.jpg',2),(6,'BERNARDO','SEGUI','$2a$10$DPmpTdssreSONSFkCyb4duFCuE6UezpfJOAuc1z5zm6KqVgTHtOOq','b@b.com','user-1639855839487_img.jpg',2),(7,'cacomaster','admin','$2a$10$L6muOHU73IvGLGdO8ZqOjeVlQYKtizAms5Z4UUsLCTjtHsFvXdBW2','admin@admin.com','user-1639856114559_img.jpg',1),(8,'b','b','$2a$10$d6NS6J8hg7.mwmOGI0Zzjejmhm6pB6Zv8Ug/Z/55WTA0yrfQS6B2u','b@ahorasi.com','hareeeee.jpg',2),(9,'pendorcho','pendorcho','$2a$10$t4B62qgvLUxPgjC4ZbKRc.LtANrA9J5BAHmB1Slm8vMkoNXHG7lkO','pendorcho@pendorcho.com','user-1640050261059_img.jpg',2),(10,'aaaaaaaa','qqqqqqq','$2a$10$5jTdtjK9nr9H8XQE3BjTMOxd3CuzH1smTjCsbZTe0C3lPZES8LZ6O','aa@qq.com','user-1640201933624_img.jpg',2),(13,'BERNARDO','SEGUI','$2a$10$FwIRLpvW7LMcnm4nSk0RseXZiTajz8f.D.319bmFOLoxaJlWICrgO','bernardo.segui@gil.com','user-1640203298108_img.jpg',2),(14,'www','wwww','$2a$10$jhvQMFuPG4fCj3rjIu9ZlOWWd0Og0icpTorOE5Ng.IXF4P9Ql0DP2','www@www.com','user-1640203853125_img.jpg',1);
+INSERT INTO `users` VALUES (15,'Charlie','Bucket','$2a$10$Z8AZSjrSFxFySVznknnqyuVSpLQ2eyUofaWASiK0rxrtBIg6IDJT6','charlie@bucket.com','user-1644182882059_img.jpg',2),(16,'Augustus','Gloop','$2a$10$rLGaT1RANmad9Al8NFv19euTwa442fBqtnjmjd.sgegFV.lyRMj/m','augustus@gloop.com','user-1644182953106_img.jpg',2),(17,'Veruca','Salt','$2a$10$wjzUV1Sh5JoZf1mX4RPwau4rOlGjBwNbWgNAIdbsGY2I0rBpnW0vi','veruca@salt.com','user-1644183037894_img.jpg',2),(18,'Violet','Beauregarde','$2a$10$t4uy8SLGuBXzbUH7EsbcGeSRQEFtovsKcTqMRbA1mGAhZL0J2ovNK','violet@beauregarde.com','user-1644183171576_img.jpeg',2),(19,'Mike','Teavee','$2a$10$8E.jtp.HyTxBsKyWQoxabO6sHVW5i61EOzj6ich9/1nPEiKhlGHea','mike@teavee.com','user-1644183254412_img.jpg',2),(21,'Willy','Wonka','$2a$10$k43NnskwMOi0o4z11NpNv.gzkE8Dv97SMsTOfoZ518FtRwvZmY04O','admin@admin.com','user-1644183654632_img.jpg',1);
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -82,4 +82,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-12-22 19:03:45
+-- Dump completed on 2022-02-06 19:08:47
