fca375d9d3fc86109735a22211613a9d82885160
ignacioiriondo <nachoiriondo90@gmail.com>
2022-02-06 10:23:00 -0300

    Header

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -2,21 +2,21 @@ module.exports= {
   "development": {
     "username": "root",
     "password": null,
-    "database": "huara",
+    "database": "huara_db",
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
   "test": {
     "username": "root",
     "password": null,
-    "database": "huara",
+    "database": "huara_db",
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
   "production": {
     "username": "root",
     "password": null,
-    "database": "huara",
+    "database": "huara_db",
     "host": "127.0.0.1",
     "dialect": "mysql"
   }
