aadef960499a67c4ca5bd71e3cef79476cfeaa6a
victoriacutino <victoria.cutino@gmail.com>
2022-02-06 11:27:47 -0300

    config

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -2,14 +2,14 @@ module.exports= {
   "development": {
     "username": "root",
     "password": null,
-    "database": "huara",
+    "database": "huara_db",
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
   "test": {
     "username": "root",
     "password": null,
-    "database": "huara",
+    "database": "huara_db",
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
